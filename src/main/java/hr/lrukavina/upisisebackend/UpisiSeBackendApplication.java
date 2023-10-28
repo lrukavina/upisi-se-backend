8db90f30abc6a30b1d0f46810a6b686e63b609c1
lrukavina <77284546+lrukavina@users.noreply.github.com>
2023-10-28 18:33:06 +0200

    init projekta

---
+++ src/main/java/hr/lrukavina/upisisebackend/UpisiSeBackendApplication.java
@@ -0,0 +1,13 @@
+package hr.lrukavina.upisisebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpisiSeBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UpisiSeBackendApplication.class, args);
+    }
+
+}
