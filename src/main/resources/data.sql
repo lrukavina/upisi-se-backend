d31f4fc28280695c7aa1c29900faf2fb6fa1fd7a
lrukavina <77284546+lrukavina@users.noreply.github.com>
2023-10-31 18:35:12 +0100

    implementiran model baze podataka

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,85 @@
-insert into korisnik(ime, prezime, email, korisnicko_ime, lozinka, rola) values
-('Luka', 'Rukavina', 'lrukavina@tvz.hr', 'lrukavina', '123', 'ADMIN');
+insert into korisnik(ime, prezime, jmbag, adresa, email, korisnicko_ime, lozinka, rola) values
+('Luka', 'Rukavina', null, null, 'lrukavina@tvz.hr', 'lrukavina', '$2a$10$AmPJwsPTr14JM16zGoxeEOBEYPWcaFA.liTXvR5jionq4YUP4/rru', 'ADMIN');
+
+insert into visoko_uciliste(naziv, adresa, postanski_broj, mjesto, iban, oib) values
+('Tehničko veleučilište u Zagrebu', 'Vrbik 8', '10000', 'Zagreb', 'HR1723600001101234565', '08814003451'),
+('Fakultet političkih znanosti', 'Lepušićeva 6', '10000', 'Zagreb', 'HR1723600001101234565', '08814003451'),
+('Fakultet prometnih znanosti', 'Vukelićeva 4', '10000', 'Zagreb', 'HR1723600001101234565', '08814003451'),
+('Filozofski fakultet', 'Ivana Lučića 3', '10000', 'Zagreb', 'HR1723600001101234565', '08814003451'),
+('Stomatološki fakultet', 'Gundulićeva 5', '10000', 'Zagreb', 'HR1723600001101234565', '08814003451'),
+('Medicinski fakultet', 'Šalata 3b', '10000', 'Zagreb', 'HR1723600001101234565', '08814003451'),
+('Geodetski fakultet', 'Kačićeva 26', '10000', 'Zagreb', 'HR1723600001101234565', '08814003451'),
+('Kineziološki fakultet', 'Horvaćanski zavoj 15', '10000', 'Zagreb', 'HR1723600001101234565', '08814003451'),
+('Muzička akademija', 'Trg Republike Hrvatske 12', '10000', 'Zagreb', 'HR1723600001101234565', '08814003451'),
+('Prirodoslovno-matematički fakultet-matematički odsjek', 'Bijenička cesta 30', '10000', 'Zagreb', 'HR1723600001101234565', '08814003451');
+
+insert into studij(naziv_studija, naziv_smjera, ects_cijena, visoko_uciliste_id) values
+('Stručni prijediplomski studij Računarstvo', 'Programsko inženjerstvo', 33, 1),
+('Stručni prijediplomski studij Računarstvo', 'Inženjerstvo računalnih sustava i mreža', 33, 1),
+('Stručni diplomski studij Digitalna ekonomija', 'Digitalna ekonomija', 33, 1),
+('Stručni prijediplomski studij Graditeljstvo', 'Građevinsko poduzetništvo', 33, 1),
+('Stručni prijediplomski studij Graditeljstvo', 'Niskogradnja', 33, 1),
+('Stručni prijediplomski studij Mehatronika', 'Mehatronika', 33, 1),
+('Stručni prijediplomski studij Strojarstvo', 'Strojarstvo', 33, 1),
+('Stručni prijediplomski studij Informatika', 'Organizacija i informatizacija ureda', 33, 1),
+('Stručni prijediplomski studij Informatika', 'Elektroničko poslovanje', 33, 1),
+('Stručni prijediplomski studij Informatika', 'Informatički dizajn', 33, 1);
+
+insert into kolegij(naziv, ects, semestar, isvu_sifra, obavezan, studij_id) values
+('NOSQL i napredna big data analitika', 6, 3, '130969', 1, 1),
+('Alati i postupci umjetne inteligencije', 6, 3, '130970', 1, 1),
+('Reinžinjering poslovnih procesa', 6, 3, '130971', 1, 1),
+('Projektiranje inovacija', 6, 3, '130972', 0, 1),
+('Napredne tehnike projektiranja web servisa', 6, 3, '130973', 0, 1),
+('Razvoj mobilnih aplikacija', 6, 3, '130974', 0, 1),
+('Modeliranje i administracija baza podataka', 6, 3, '130974', 0, 1),
+('Digitalna ekonomija', 5, 1, '130975', 1, 1),
+('Diskretna matematika', 6, 2, '130976', 0, 1),
+('Metodologija stručnog i istraživačkog rada', 6, 4, '130977', 1, 1);
+
+insert into kolegij_info(informacije, kolegij_id) values
+('Informacije o kolegiju nisu dostupne.', 1),
+('Informacije o kolegiju nisu dostupne.', 2),
+('Informacije o kolegiju nisu dostupne.', 3),
+('Informacije o kolegiju nisu dostupne.', 4),
+('Informacije o kolegiju nisu dostupne.', 5),
+('Informacije o kolegiju nisu dostupne.', 6),
+('Informacije o kolegiju nisu dostupne.', 7),
+('Informacije o kolegiju nisu dostupne.', 8),
+('Informacije o kolegiju nisu dostupne.', 9),
+('Informacije o kolegiju nisu dostupne.', 10);
+
+insert into kolegij_nastavnik(ime, prezime, titula, kolegij_id) values
+('John', 'Doe', 'dr.sc.', 1),
+('John', 'Doe', 'dr.sc.', 2),
+('John', 'Doe', 'dr.sc.', 3),
+('John', 'Doe', 'dr.sc.', 4),
+('John', 'Doe', 'dr.sc.', 5),
+('John', 'Doe', 'dr.sc.', 6),
+('John', 'Doe', 'dr.sc.', 7),
+('John', 'Doe', 'dr.sc.', 8),
+('John', 'Doe', 'dr.sc.', 9),
+('John', 'Doe', 'dr.sc.', 10);
+
+insert into upis(semestar, tstamp_od, tstamp_do, studij_id) values
+(3, '2023-09-01 00:00:00', '2023-09-30 00:00:00', 1),
+(3, '2023-09-01 00:00:00', '2023-09-30 00:00:00', 2),
+(3, '2023-09-01 00:00:00', '2023-09-30 00:00:00', 3),
+(3, '2023-09-01 00:00:00', '2023-09-30 00:00:00', 4),
+(3, '2023-09-01 00:00:00', '2023-09-30 00:00:00', 5),
+(3, '2023-09-01 00:00:00', '2023-09-30 00:00:00', 6),
+(3, '2023-09-01 00:00:00', '2023-09-30 00:00:00', 7),
+(3, '2023-09-01 00:00:00', '2023-09-30 00:00:00', 8),
+(3, '2023-09-01 00:00:00', '2023-09-30 00:00:00', 9),
+(3, '2023-09-01 00:00:00', '2023-09-30 00:00:00', 10);
+
+insert into upis_info(poziv_na_broj, broj_ectsa, upisni_broj, opis, status, upis_id, korisnik_id) values
+('370-0246073114-7', 30, 'UB0001', 'Školarina zimski semestar 2023/2024 UB0001', 'U_TIJEKU', 1, 1);
+
+insert into upis_info_kolegij(upis_info_id, kolegij_id) values
+(1, 1),
+(1, 2),
+(1, 3),
+(1, 4),
+(1, 5);
+
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -2,8 +2,86 @@ create table if not exists korisnik(
     id identity,
     ime varchar(255) not null,
     prezime varchar(255) not null,
+    jmbag varchar(10),
+    adresa varchar(255),
     email varchar(255) not null,
     korisnicko_ime varchar(255) not null,
     lozinka varchar(255) not null,
-    rola varchar(255) not null
+    rola varchar(255) not null,
+    visoko_uciliste_id bigint
+);
+
+create table if not exists visoko_uciliste(
+    id identity,
+    naziv varchar(255) not null,
+    adresa varchar(255) not null,
+    postanski_broj varchar(255) not null,
+    mjesto varchar(255) not null,
+    iban varchar(21) not null,
+    oib varchar(11) not null
+);
+
+create table if not exists studij(
+    id identity,
+    naziv_studija varchar(255) not null,
+    naziv_smjera varchar(255) not null,
+    ects_cijena int not null,
+    visoko_uciliste_id bigint not null,
+    constraint fk_visoko_uciliste foreign key (visoko_uciliste_id) references visoko_uciliste(id)
+);
+
+create table if not exists kolegij(
+    id identity,
+    naziv varchar(255) not null,
+    ects int not null,
+    semestar int not null,
+    isvu_sifra varchar(255) not null,
+    obavezan int not null,
+    studij_id bigint not null,
+    constraint fk_studij foreign key (studij_id) references studij(id)
+);
+
+create table if not exists kolegij_info(
+    id identity,
+    informacije text not null,
+    kolegij_id bigint not null,
+    constraint fk_kolegij_info foreign key (kolegij_id) references kolegij(id)
+);
+
+create table if not exists kolegij_nastavnik(
+    id identity,
+    ime varchar(255) not null,
+    prezime varchar(255) not null,
+    titula varchar(255) not null,
+    kolegij_id bigint not null,
+    constraint fk_kolegij_nastavnik foreign key (kolegij_id) references kolegij(id)
+);
+
+create table if not exists upis(
+    id identity,
+    semestar int not null,
+    tstamp_od timestamp not null,
+    tstamp_do timestamp not null,
+    studij_id bigint not null,
+    constraint fk_studij_upis foreign key (studij_id) references studij(id)
+);
+
+create table if not exists upis_info(
+    id identity,
+    poziv_na_broj varchar(255) not null,
+    broj_ectsa int not null,
+    upisni_broj varchar(255) not null,
+    opis varchar(255) not null,
+    status varchar(255) not null,
+    upis_id bigint not null,
+    korisnik_id bigint not null,
+    constraint fk_upis foreign key (upis_id) references upis(id),
+    constraint fk_korisnik foreign key (korisnik_id) references korisnik(id)
+);
+
+create table if not exists upis_info_kolegij(
+    upis_info_id bigint not null,
+    kolegij_id bigint not null,
+    constraint fk_upis_info foreign key (upis_info_id) references upis_info(id),
+    constraint fk_kolegij foreign key (kolegij_id) references kolegij(id)
 );
