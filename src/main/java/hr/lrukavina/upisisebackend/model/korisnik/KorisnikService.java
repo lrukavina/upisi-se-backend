e66eaa1efd5ffeb065aae58b748ea171eca52f76
lrukavina <77284546+lrukavina@users.noreply.github.com>
2023-10-31 22:18:35 +0100

    servisne metode

--- src/main/java/hr/lrukavina/upisisebackend/model/korisnik/KorisnikService.java
+++ src/main/java/hr/lrukavina/upisisebackend/model/korisnik/KorisnikService.java
@@ -1,5 +1,12 @@
 package hr.lrukavina.upisisebackend.model.korisnik;
 
+import hr.lrukavina.upisisebackend.model.korisnik.request.AzurirajKorisnikaRequest;
+
 public interface KorisnikService {
-    Korisnik dohvatiPoKorisnickomImenu(String korisnickoIme);
+  // todo vratiti DTO za kontroler
+  Korisnik dohvatiPoKorisnickomImenu(String korisnickoIme);
+
+  Korisnik azuriraj(AzurirajKorisnikaRequest request, String korisnickoIme);
+
+  void izbrisi(String korisnickoIme);
 }
