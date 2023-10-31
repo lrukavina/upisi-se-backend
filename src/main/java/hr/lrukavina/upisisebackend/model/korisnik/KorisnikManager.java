6989e6cbc33d671ee37235bea37489c815c8bccf
lrukavina <77284546+lrukavina@users.noreply.github.com>
2023-10-31 22:17:06 +0100

    manager metode

--- src/main/java/hr/lrukavina/upisisebackend/model/korisnik/KorisnikManager.java
+++ src/main/java/hr/lrukavina/upisisebackend/model/korisnik/KorisnikManager.java
@@ -8,11 +8,29 @@ import org.springframework.stereotype.Service;
 public class KorisnikManager {
   private final KorisnikRepository repository;
 
-  public Korisnik dohvatiPoKorisnickomImenu(String korisnickoIme) {
+  public Korisnik dohvati(Integer korisnikId) {
+    return repository.dohvati(korisnikId);
+  }
+
+  public Korisnik dohvati(String korisnickoIme) {
     return repository.dohvatiPoKorisnickomImenu(korisnickoIme);
   }
 
   public void spremi(Korisnik korisnik) {
     repository.spremi(korisnik);
   }
+
+  public void azuriraj(Korisnik korisnik) {
+    repository.azuriraj(korisnik);
+  }
+
+  public void izbrisi(String korisnickoIme) {
+    repository.izbrisiPoKorisnickomImenu(korisnickoIme);
+  }
+
+  public void izbrisi(Integer korisnikId) {
+    repository.izbrisi(korisnikId);
+  }
+
+  // todo izbrisi sve iz ostalih tablica
 }
