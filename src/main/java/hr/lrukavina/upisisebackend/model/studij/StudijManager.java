cb9e0c37d73daf3047524ec726994a38caabee8e
lrukavina <77284546+lrukavina@users.noreply.github.com>
2023-11-01 15:37:55 +0100

    studij manager

---
+++ src/main/java/hr/lrukavina/upisisebackend/model/studij/StudijManager.java
@@ -0,0 +1,26 @@
+package hr.lrukavina.upisisebackend.model.studij;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class StudijManager {
+  private final StudijRepository repository;
+
+  public Studij dohvati(Integer studijId) {
+    return repository.dohvati(studijId);
+  }
+
+  public void spremi(Studij studij) {
+    repository.spremi(studij);
+  }
+
+  public void azuriraj(Studij studij) {
+    repository.azuriraj(studij);
+  }
+
+  public void izbrisi(Integer studijId) {
+    repository.izbrisi(studijId);
+  }
+}
