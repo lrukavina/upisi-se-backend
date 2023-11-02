3194b7fe2281b5f113f822d8b68fccb04983a162
lrukavina <77284546+lrukavina@users.noreply.github.com>
2023-11-02 20:27:12 +0100

    model za upis

---
+++ src/main/java/hr/lrukavina/upisisebackend/model/upis/Upis.java
@@ -0,0 +1,20 @@
+package hr.lrukavina.upisisebackend.model.upis;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Upis {
+    private Integer id;
+    private Integer semestar;
+    private LocalDateTime tstampOd;
+    private LocalDateTime tstampDo;
+    private Integer studijId;
+}
