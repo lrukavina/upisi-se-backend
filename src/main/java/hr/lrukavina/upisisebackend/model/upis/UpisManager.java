a725e0ce906c219a39e5f9e5c01d91d9fb6418f2
lrukavina <77284546+lrukavina@users.noreply.github.com>
2023-11-02 21:07:34 +0100

    manager za upis i upisInfo

---
+++ src/main/java/hr/lrukavina/upisisebackend/model/upis/UpisManager.java
@@ -0,0 +1,26 @@
+package hr.lrukavina.upisisebackend.model.upis;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UpisManager {
+  private final UpisRepository repository;
+
+  public Upis dohvati(Integer upisId) {
+    return repository.dohvati(upisId);
+  }
+
+  public void spremi(Upis upis) {
+    repository.spremi(upis);
+  }
+
+  public void azuriraj(Upis upis) {
+    repository.azuriraj(upis);
+  }
+
+  public void izbrisi(Integer upisId) {
+    repository.izbrisi(upisId);
+  }
+}
